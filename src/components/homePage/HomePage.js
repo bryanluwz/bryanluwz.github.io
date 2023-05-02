8c95fa777bd15303a327347c92505e8001de0d59
Bryan <bryanlu.my@gmail.com>
2023-05-02 14:37:15 +0800

    header and body skeleton

---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,18 @@
+import { Component, Fragment } from "react";
+import MainTitleCard from '../mainTitleCard/MainTitleCard';
+import TitleCard from "../titleCard/TitleCard";
+
+export default class HomePage extends Component {
+	render() {
+		return (
+			<Fragment>
+				<MainTitleCard>
+
+				</MainTitleCard>
+				<TitleCard>
+
+				</TitleCard>
+			</Fragment>
+		);
+	}
+} 
